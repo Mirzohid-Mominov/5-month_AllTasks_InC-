01f8841177c00478db8d4b42ff31684e23e62f8f
Mirzohid-Mominov <momirzohid4@gmail.com>
2023-12-03 19:29:39 +0500

    created Architecture for OddiyBlogSayt HT

---
+++ OddiyBlogSayt_HT/Program.cs
@@ -0,0 +1,15 @@
+namespace OddiyBlogSayt_HT
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
